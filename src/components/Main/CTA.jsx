4c0475e6fb4bb00b362ff98b3c64941e59d8092f
Vedran Mikulček <Vedran.Mikulcek@iskon.hr>
2023-06-07 00:13:38 +0200

    Downgrade to React 17 for enabling Material UI icon dependency compatibility.

--- src/components/Main/CTA.jsx
+++ src/components/Main/CTA.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const CTA = () => {
     return (
-        <div className="cta flex justify-center space-x-2">
-            <a href="#" className="bg-blue-500 text-white rounded-md px-8 py-2 inline-block">Get started with ØRbit.Ui</a>
-            <span className="self-center text-gray-600">or</span>
-            <a href="#" className="text-blue-500">See how it works</a>
+        <div className="cta flex justify-center space-x-2 mb-2">
+            <a href="#" className="bg-blue-500 text-white rounded-md px-8 py-2 inline-block">
+                Get started with ØRbit.Ui
+            </a>
+            <span className="self-center text-gray-600">
+                or
+            </span>
+            <a href="#" className="text-blue-500">
+                See how it works
+            </a>
         </div>
     );
 }
--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <section className="hero text-center py-16">
             <h1 className="text-7xl font-bold text-gray-800 mb-4">AI-Powered Line Parameter Analytics</h1>
-            <p className="text-4xl font-work-sans">Unleash Better Customer Care with AI Simplified Line Insights</p>
+            <p className="text-4xl font-work-sans mb-16">Unleash Better Customer Care with AI Simplified Line Insights</p>
             {/* Add the rest of the content here */}
             <div className="flex flex-wrap justify-center space-x-5">
                 <Card image="random-icon1.png" alt="icon1" description="Babysitting of Telecommunication Line" />
