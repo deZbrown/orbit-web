a5a8f971cb3351c6656e5d1030e070725f2be737
Vedran Mikulček <Vedran.Mikulcek@iskon.hr>
2023-06-07 18:53:46 +0200

    Small adaptation on Header and CTX.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import orbitLogo from '../assets/orbit-logo.png';
 const Header = () => {
     return (
         <header className="flex justify-between items-center py-6">
-            <div className="logo">
+            <div className="logo drop-shadow-xl">
                 <img src={orbitLogo} alt="ØRbit.Ui logo" className="w-16 h-16 object-cover rounded-lg"/>
             </div>
             <nav className="flex-grow">
@@ -36,7 +36,7 @@ const Header = () => {
                     </li>
                 </ul>
             </nav>
-            <a href="#" className="bg-blue-500 text-white rounded-md px-2 h-14 flex items-center justify-center">
+            <a href="#" className="bg-blue-500 font-lato text-white text-lg rounded-md px-2 h-14 flex items-center justify-center drop-shadow-xl">
                 Get started
             </a>
         </header>
--- src/components/Main/CTA.jsx
+++ src/components/Main/CTA.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const CTA = () => {
     return (
         <div className="cta flex justify-center space-x-2 mb-2">
-            <a href="#" className="bg-blue-500 text-white rounded-md px-8 py-2 h-14 flex items-center justify-center">
-                Get started with ØRbit.Ui
+            <a href="#" className="bg-blue-500 font-lato text-white text-xl rounded-md px-8 py-2 h-14 flex items-center justify-center drop-shadow-xl">
+                Get in contact
             </a>
             <span className="self-center text-gray-600">
                 or
             </span>
-            <a href="#" className="bg-blue-500 text-white rounded-md px-8 py-2 flex items-center justify-center">
-                See how it works
+            <a href="#" className="bg-blue-500 font-lato text-white text-xl rounded-md px-8 py-2 flex items-center justify-center drop-shadow-xl">
+                Get stated
             </a>
         </div>
     );
