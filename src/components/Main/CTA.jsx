ac106019118d6d61d63ae30489e101291734d0e7
Vedran Mikulček <Vedran.Mikulcek@iskon.hr>
2023-06-10 16:23:21 +0200

    Update CTA for better fitment with mobile screen size.

--- src/components/Main/CTA.jsx
+++ src/components/Main/CTA.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CTA = () => {
     return (
-        <div className="mt-6 flex max-w-lg gap-4 justify-center mx-auto">
+        <div className="my-24 flex max-w-lg gap-1 justify-center mx-auto">
             <label htmlFor="email-address" className="sr-only">Email address</label>
             <input
                 id="email-address"
@@ -10,7 +10,7 @@ const CTA = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className="min-w-0 flex-auto rounded-md border-gray-300 bg-white px-4 py-4 text-lg text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md border-2 border-gray-300 sm:border-1 bg-white px-4 py-4 text-lg text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 placeholder="Enter your email"
             />
             <button
@@ -20,6 +20,7 @@ const CTA = () => {
                 Subscribe
             </button>
         </div>
+
     );
 }
 
