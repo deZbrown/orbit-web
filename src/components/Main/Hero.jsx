f526a136b4686cb7617efd0479f1abe1cb1e0162
Vedran Mikulček <Vedran.Mikulcek@iskon.hr>
2023-06-10 16:24:19 +0200

    Disabled all feature sections, to focus on refining of feature segment for mobile screen size.

--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -12,30 +12,30 @@ const Hero = () => {
     return (
         <section className="hero text-center py-16">
 
-            <h1 className="text-7xl font-bold text-gray-800 mb-4">AI-Powered Line Parameter Analytics</h1>
-            <p className="text-4xl font-work-sans mb-16">Unleash Better Customer Care with AI Simplified Line Insights</p>
+            <h1 className="text-5xl md:text-7xl font-bold text-gray-800 mb-8">AI-Powered Line Parameter Analytics</h1>
+            <p className="text-4xl font-work-sans mb-16 mt-12">Unleash Better Customer Care with AI Simplified Line Insights</p>
 
-            <div className="flex flex-wrap justify-center space-x-5 mb-16">
-                <Card iconName="alarm" description="Babysitting of Telecommunication Line" />
-                <Card iconName="account-balance" description="Telecommunication Line Performance Score" />
-                <Card iconName="alarm" description="Predictive Analytics for Telecommunication Line Health" />
-                <Card iconName="add-to-queue" description="Sophisticated Optical Network Analysis" />
-                <Card iconName="all-inclusive" description="Customer Service Diagnosis" />
-            </div>
+            {/*<div className="flex flex-wrap justify-center space-x-5 mb-16">*/}
+            {/*    <Card iconName="alarm" description="Babysitting of Telecommunication Line" />*/}
+            {/*    <Card iconName="account-balance" description="Telecommunication Line Performance Score" />*/}
+            {/*    <Card iconName="alarm" description="Predictive Analytics for Telecommunication Line Health" />*/}
+            {/*    <Card iconName="add-to-queue" description="Sophisticated Optical Network Analysis" />*/}
+            {/*    <Card iconName="all-inclusive" description="Customer Service Diagnosis" />*/}
+            {/*</div>*/}
 
             <CTA />
 
-            <DataDrivenDecisionMaking />
+            {/*<DataDrivenDecisionMaking />*/}
 
-            <DiagnosisAndPredictiveCapabilities />
+            {/*<DiagnosisAndPredictiveCapabilities />*/}
 
             <ReducedOperatingCosts />
 
-            <ContinuousInnovation />
+            {/*<ContinuousInnovation />*/}
 
-            <ImprovedServiceAgentEfficiency />
+            {/*<ImprovedServiceAgentEfficiency />*/}
 
-            <ScalableAndAdaptableSolution />
+            {/*<ScalableAndAdaptableSolution />*/}
 
         </section>
     );
--- src/components/Main/SVP/ReducedOperatingCosts.jsx
+++ src/components/Main/SVP/ReducedOperatingCosts.jsx
@@ -3,25 +3,28 @@ import costs from '../../../assets/costs.png';
 
 const FeatureSection = () => {
     return (
-        <div className="flex flex-col md:flex-row w-full justify-between my-14 px-8">
+        <div className="flex flex-col md:flex-row max-w-7xl mx-auto justify-around my-16 px-4 md:px-12 items-center">
 
-            <div className="w-full md:w-1/2 flex flex-col justify-center items-start px-12 mb-8 md:mb-0">
+            <div className="w-full md:w-1/2 flex flex-col justify-center items-start px-3 md:px-12 mb-12 md:mb-0">
 
-                <h2 className="mb-4 text-6xl font-work-sans tracking-tight text-gray-900 dark:text-white text-left">
-                    Reduced Operating Costs
+                <h2 className="mb-4 text-5xl md:text-6xl font-work-sans tracking-tight text-gray-900 dark:text-white text-left">
+                    Smart Cost Management
                 </h2>
 
                 <p className="font-thin font-work-sans text-2xl text-gray-700 leading-relaxed text-justify mt-4 italic">
-                    Our API adapts to your needs with its flexible pay-as-you-go model.
                     Experience true financial freedom and only pay for what you use.
+                </p>
+
+                <p className="font-thin font-work-sans text-2xl text-gray-700 leading-relaxed text-justify mt-4 italic">
+                    Our API adapts to your needs with its flexible Pay-as-You-Go model.
                 </p>
 
             </div>
 
-            <div className="w-full md:w-1/2 flex justify-center px-24">
-
-                <img src={costs} alt="Visual" className="rounded-lg" />
-
+            <div className="w-full md:w-1/2 flex justify-center md:max-w-none">
+                <div className="mx-auto">
+                    <img src={costs} alt="Visual" className="rounded-lg max-w-full" />
+                </div>
             </div>
 
         </div>
